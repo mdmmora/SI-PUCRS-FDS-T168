5512a1a547fa68682a3a6bd3d2e5c83a41320e60
DESKTOP-3G3KIBU\mcmor <michael.c.mora@gmail.com>
2017-08-29 19:13:31 -0300

    Inserindo as instruções.

--- SI-PUCRS-FDS-T168/src/FDSMain.java
+++ SI-PUCRS-FDS-T168/src/FDSMain.java
@@ -2,6 +2,10 @@
 public class FDSMain {
 
 	public static void main(String[] args) {
+		//*******************
+		// Insira, no final deste método, um comentário com 
+		// o seu nome, e um println com o Hello, World, conform
+		// o exemplo abaixo.
 		System.out.println("Hello, World! Comunitario!\n");
 
 		// Michael da Costa Móra
