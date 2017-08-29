05ced7666ca5a9501acd122843c3839df7fcead4
16105038 <16105038@l3043213.portoalegre.pucrsnet.br>
2017-08-29 20:27:40 -0300

    Henrique

--- SI-PUCRS-FDS-T168/src/FDSMain.java
+++ SI-PUCRS-FDS-T168/src/FDSMain.java
@@ -3,13 +3,15 @@ public class FDSMain {
 
 	public static void main(String[] args) {
 		//*******************
-		// Insira, no final deste método, um comentário com 
+		// Insira, no final deste mï¿½todo, um comentï¿½rio com 
 		// o seu nome, e um println com o Hello, World, conform
 		// o exemplo abaixo.
 		System.out.println("Hello, World! Comunitario!\n");
 
-		// Michael da Costa Móra
+		// Michael da Costa Mï¿½ra
 		System.out.println("Hello, World! Comunitario! Michael\n");
+		// Henrique Sangalli CorrÃªa
+		System.out.println("Hello, World! Comunitario! Henrique\n");
 	}
 
 }
