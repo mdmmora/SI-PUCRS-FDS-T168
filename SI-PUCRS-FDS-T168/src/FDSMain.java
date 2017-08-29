87b17fa57a0719773a21024a89edd9cffcba30ab
DESKTOP-3G3KIBU\mcmor <michael.c.mora@gmail.com>
2017-08-29 19:10:10 -0300

    versão inicial.

---
+++ SI-PUCRS-FDS-T168/src/FDSMain.java
@@ -0,0 +1,11 @@
+
+public class FDSMain {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, World! Comunitario!\n");
+
+		// Michael da Costa Móra
+		System.out.println("Hello, World! Comunitario! Michael\n");
+	}
+
+}
