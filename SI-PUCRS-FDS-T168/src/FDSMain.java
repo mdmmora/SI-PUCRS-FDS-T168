7eaac541b1053b06e26b5fb8ec5ad55bc398a412
unknown <13205039@W3043312.portoalegre.pucrsnet.br>
2017-08-29 20:56:56 -0300

    Hello world

--- SI-PUCRS-FDS-T168/src/FDSMain.java
+++ SI-PUCRS-FDS-T168/src/FDSMain.java
@@ -2,26 +2,17 @@
 public class FDSMain {
 
 	public static void main(String[] args) {
-		//*******************
-<<<<<<< HEAD
-		// Insira, no final deste m�todo, um coment�rio com 
-=======
-		
-		
-		
-		// Insira, no final deste método, um comentário com 
 
->>>>>>> e8d467e5dc0078e82d3fc5c64c52e39a410025ea
+		// Insira, no final deste m�todo, um coment�rio com 
+		// Insira, no final deste método, um comentário com
+
+
 		// o seu nome, e um println com o Hello, World, conform
 		// o exemplo abaixo.
 		System.out.println("Hello, World! Comunitario!\n");
 
-<<<<<<< HEAD
 		// Michael da Costa M�ra
 		System.out.println("Hello, World! Comunitario! Michael\n");
-		
-	
-=======
 		
 		// Michael da Costa M�ra
 		System.out.println("Hello, World! Comunitario! Michael\n");
@@ -52,8 +43,8 @@ public class FDSMain {
 		//Rodrigo Gonçalves
 		System.out.println("Hello, World! Comunitario! Rodrigo\n");
 
-
->>>>>>> e8d467e5dc0078e82d3fc5c64c52e39a410025ea
+		//Camila Moser
+		System.out.println("Hello, World! Comunitario! Camila\n");	
 	}
 
 }
